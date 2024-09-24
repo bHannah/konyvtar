d81b57626dea3bfdbf67e561b07183f6cd322d9e
bHannah <borsikaaa1@gmail.com>
2024-09-24 15:50:47 +0200

    orai

--- konyvtar/database/seeders/DatabaseSeeder.php
+++ konyvtar/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Copy;
+use App\Models\LibUser;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        LibUser::factory(10)->create();
+        Book::factory(25)->create();
+        Copy::factory(10)->create();
+        // LibUser::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
